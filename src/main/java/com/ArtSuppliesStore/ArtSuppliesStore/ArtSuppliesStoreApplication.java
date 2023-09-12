d3a981c487e05cc215142bf98dab5710bafd97b1
Hariprashanth75 <56840028+Hariprashanth75@users.noreply.github.com>
2023-09-12 20:43:15 -0300

    Spring project starter

---
+++ src/main/java/com/ArtSuppliesStore/ArtSuppliesStore/ArtSuppliesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ArtSuppliesStore.ArtSuppliesStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSuppliesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtSuppliesStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArtSuppliesStore/ArtSuppliesStore/ArtSuppliesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArtSuppliesStore.ArtSuppliesStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtSuppliesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
